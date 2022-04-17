9dc2ec609a4ced25615713d586812ed2fe0f56ce
Makerstudi <85789595+makerstudi@users.noreply.github.com>
2022-04-17 12:25:16 +0900

    Update HealthCommand.lua

--- HealthCommand.lua
+++ HealthCommand.lua
@@ -14,6 +14,7 @@ local plrs = game:GetService("Players")
 local plr = plrs.LocalPlayer
 local char = plr.Character
 local place = game.PlaceId
+local Logo = "rbxassetid://2093843497"
 local prefix = "/server:"
 local Health = 0
 
@@ -31,6 +32,32 @@ plr.Chatted:Connect(function(msg)
         wait(0.2)
         game.StarterGui:SetCore("ChatMakeSystemMessage",{
             Text = "[Server]: 현재 접두사: '" .. prefix .. "' 입니다.",
+            Font = Enum.Font.SourceSansBold,
+            Color = Color3.fromRGB(0, 120, 255),
+            FontSize = Enum.FontSize.Size96
+        })
+    elseif string.sub(msg, 1, 14) == prefix .. "rejoin" then
+        wait(0.2)
+        game.StarterGui:SetCore("ChatMakeSystemMessage",{
+            Text = "[Server]: 3초뒤 Rejoin",
+            Font = Enum.Font.SourceSansBold,
+            Color = Color3.fromRGB(0, 120, 255),
+            FontSize = Enum.FontSize.Size96
+        })
+        wait(3)
+        game:GetService("TeleportService"):Teleport(game.PlaceId, player)
+        
+    elseif string.sub(msg, 1, 11) == prefix .. "dev" then
+        game.StarterGui:SetCore("SendNotification", {
+            Title = "by";
+            Text = "Script owner: TW_ASVZ,SynapseX Scripter";
+            icon = Logo;
+            Duration = 10;
+            Button1 = "Check"
+        })
+        wait(0.2)
+        game.StarterGui:SetCore("ChatMakeSystemMessage",{
+            Text = "[Server]: Script owner: TW_ASVZ,SynapseX Scripter",
             Font = Enum.Font.SourceSansBold,
             Color = Color3.fromRGB(0, 120, 255),
             FontSize = Enum.FontSize.Size96
